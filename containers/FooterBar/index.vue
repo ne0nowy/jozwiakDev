62f9dd9563686abcc5699196c52a0dcd5f51f5fe
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2022-11-02 03:49:52 +0100

    create(container): Footer container

---
+++ containers/FooterBar/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mx-auto">
+        <div class="grid grid-cols-3 text-center">
+            <div>Links</div>
+            <div>Blog</div>
+            <div>Logo</div> 
+        </div>       
+        <div class="border-t gradient-border text-center py-3 mt-4">
+            © 2022 jozwiak.dev
+        </div>
+    </div>
+</template>
