a300f82418b8da9d077b857e0fc8b86ac97d0b54
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2022-11-02 03:50:12 +0100

    create(container): NavBar & NavBarMenuButton

---
+++ containers/NavBar/MenuButton/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <NuxtLink :to="to" :href="href" class="px-4 py-1 leading-normal transition duration-150 ease-in-out bg-white bg-opacity-0 hover:bg-opacity-10 rounded-3xl text-base cursor-pointer"><slot /></NuxtLink>
+</template>
+
+<script setup>
+defineProps({
+    to: String,
+    href: String,
+})
+</script>
---
+++ containers/NavBar/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="container mx-auto flex flex-row py-6">
+            Logo
+            <div class="flex-1"></div>
+            <NavBarMenuButton class="md:mx-2">Realizacje</NavBarMenuButton>
+            <NavBarMenuButton class="md:mx-2">Blog</NavBarMenuButton>
+            <NavBarMenuButton class="md:mx-2">O mnie</NavBarMenuButton>
+            <NavBarMenuButton class="md:mx-2">Kontakt</NavBarMenuButton>
+        </div>
+    </div>
+</template>
