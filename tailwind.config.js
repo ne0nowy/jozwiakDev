e8fd447c136cb44b2a50ba150fb9c7e8fd47bdb6
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2022-11-02 03:51:33 +0100

    update(tailwind.conf): add fontSize and container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,17 @@ module.exports = {
     theme: {
       container: {
         screens: {
-          "2xl": "1280px",
+          '2xl': '1280px',
         }
       },
+      fontSize: {
+        'sm': '0.8rem',
+        'base': '0.875rem',
+        'xl': '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      }
     }
 }
